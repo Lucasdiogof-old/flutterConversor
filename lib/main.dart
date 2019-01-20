545c285d7e3e629f432cad5c0e2fb3a06295015d
Lucas Diogo França <lucasdiogo1234@gmail.com>
2019-01-20 10:58:05 -0200

    Update main.dart
    Correção no cálculo do dólar.

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _HomeState extends State<Home> {
 
   void _changeDolar(String text) {
     double dolar = double.parse(text);
-    controleReal.text = (dolar / this.dolar).toStringAsFixed(2);
+    controleReal.text = (dolar * this.dolar).toStringAsFixed(2);
     controleEuro.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
